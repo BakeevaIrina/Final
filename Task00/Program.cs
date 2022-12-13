e5a039fd9875a85bb3449ec85715e9bad68ee8b8
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-12-14 00:14:08 +0600

    Creat array

---
+++ Task00/Program.cs
@@ -0,0 +1,11 @@
+﻿void Array (string []array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            Console.WriteLine(array[i] + " ");
+        }
+    }
+}
+
